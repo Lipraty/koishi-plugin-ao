3a626e79a90af93a3d14743f8f2c46a580a16c0f
Lipraty <sis1943@outlook.com>
2023-02-23 01:15:58 +0800

    fix: fix incorrect require locale file

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export interface Config { probability: number }
 export const Config: Schema<Config> = Schema.object({ probability: Schema.number().role('slider').min(1).max(100).step(1).default(30).description('吃掉的概率！') })
 
 export function apply(ctx: Context, cfg: Config) {
-    ctx.i18n.define('zh', require('./locales/zh.yaml'))
+    ctx.i18n.define('zh', require('./locales/zh'))
     ctx.before('send', (session) => {
         if (Math.ceil(Math.random() * 100) <= cfg.probability) session.elements = [h('', [session.text('ao.swallow')])] 
     })
